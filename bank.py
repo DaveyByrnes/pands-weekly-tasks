7c6f6a1ca71d093036eb7f761e592096ec901b5a
Dave Byrne <calamity.daveyjones@gmail.com>
2025-02-05 13:41:41 +0000

    started task, need to convert cents to euros

---
+++ bank.py
@@ -0,0 +1,13 @@
+# bank.py - weekly task 2
+# create the bank program required for task 2
+# by Dave Byrne :)
+
+# prompt user
+num1_cents = float(input("Enter the first amount(in cents):"))
+num2_cents = float(input("Enter the second amount(in cents):"))
+
+# add amount
+total_cents = num1_cents + num2_cents
+
+# print result
+print("The total amount is:", total_cents)

22facd8bf0bb680434ef8e753944942434237867
Dave Byrne <calamity.daveyjones@gmail.com>
2025-04-24 19:23:38 +0000

    issue with vscode - resolved through codespaces

--- plottask.py
+++ plottask.py
@@ -4,16 +4,39 @@
 # author: Dave Byrne :)
 
 import numpy as np
-import matplotlib.plyplot as plt
+import matplotlib.pyplot as plt
+
+# generate 1000 values from a normal distribution
+mean = 5
+std_dev = 2
+data = np.random.normal(mean, std_dev, 1000)
+
+# create x values for function
+x = np.linspace(0, 10, 100)
+h = x ** 3
 
 # create plot
 plt.figure(figsize=(10, 5))
-plt.hist(data, bins=30, alpha=0.5, colour='blue', label='Normal Distribution')
-plt.plot(x, y, colour='red', label='h(x)=x^3')
+plt.hist(data, bins=30, alpha=0.5, color='blue', label='Normal Distribution')
+plt.plot(x, h, color='red', label='h(x)=x^3')
 plt.title('Normal Distribution')
 plt.xlabel('x')
 plt.ylabel('Frequency')
-plt.(legend())
+plt.legend()
 plt.grid(True)
 plt.tight_layout()
 
+# show plot
+plt.show()
+
+'''
+I ran into an issue with VSCode and Python while coding this.
+It failed to find PyLance and kind of crashed Python altogether on me.
+Now that I'm using a codespace, I'm using chatgpt for the final portion
+of this code to show me how to load a png file and verify that my code is
+working on this codespace.
+'''
+
+plt.savefig('plot.png')
+
+print("Plot saved as 'plot.png'.")
